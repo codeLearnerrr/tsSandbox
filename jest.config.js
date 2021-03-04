9b4552cf90db8f74b8eb2ce2ed69f4a27248ed6c
Jeferson Eiji <jeferson.eiji@icloud.com>
2021-03-03 22:27:29 -0300

    added react-navigation  configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  "transformIgnorePatterns": [
+    "node_modules/(?!(react-native|@react-navigation|react-native-iphone-x-helper)/)"
+  ],
+  "setupFiles": [
+    "<rootDir>/jest/setup.js"
+  ],
 };
