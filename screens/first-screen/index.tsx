7bc3839703c9b9c0cc9282a64c59eb534b5ee8b4
Jeferson Eiji <jeferson.eiji@icloud.com>
2021-03-03 22:26:41 -0300

    added input and formatted layout

--- screens/first-screen/index.tsx
+++ screens/first-screen/index.tsx
@@ -1,36 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     View,
     Text,
-    Image
+    Image,
+    KeyboardAvoidingView,
+    Button
 } from 'react-native'
 import {
-    SumOperation,
+    AddOperation,
     SubtractOperation,
     DivideOperation,
     MultiplyOperation
 } from '../../components/operations'
 
 import randomImage from '../../assets/image.jpg';
+import { TextInput } from 'react-native-gesture-handler';
 
-const resultSum = SumOperation({ firstNumber: 5, secondNumber: 5 })
-const resultSub = SubtractOperation({ firstNumber: 10, secondNumber: 5 })
-const resultDiv = DivideOperation({ firstNumber: 10, secondNumber: 5 })
-const resultMult = MultiplyOperation({ firstNumber: 10, secondNumber: 5 })
 
-export const ShowSomething = () => {
+export const ShowSomething = ({ navigation }: any) => {
+    const [firstValue, setFirstValue] = useState<number>(0);
+    const [secondValue, setSecondValue] = useState<number>(0);
+
     return (
-        <>
-            <Image resizeMethod='resize' resizeMode='contain' source={randomImage} style={{ width: 50, height: 50 }} />
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>
-                    Sum: {resultSum}{'\n'}
-                Sub: {resultSub}{'\n'}
-                Div: {resultDiv}{'\n'}
-                Mult: {resultMult}{'\n'}
+        <View style={{ flex: 1, alignItems: 'center' }}>
+            {/* <Button title='go to second screen' onPress={() => navigation.navigate('NotHome')} /> */}
+
+            < Image
+                resizeMethod='resize'
+                resizeMode='contain'
+                source={randomImage}
+                style={{ width: 100, height: 100, alignSelf: 'center', borderRadius: 10, marginTop: 10 }
+                }
+            />
+
+            <Text style={{ fontSize: 30, marginBottom: 30, textAlign: 'center' }}>
+                Type numbers in{'\n'} each grey box to calculate:
                 </Text>
+
+            <View style={{ flex: 1, flexDirection: 'column', maxHeight: 100, }}>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <TextInput placeholder='' keyboardType='numeric' style={{
+                        borderColor: 'grey',
+                        borderBottomWidth: 1,
+                        backgroundColor: 'rgba(0,0,0,.1)',
+                        maxHeight: 70,
+                        maxWidth: 70,
+                        width: 70,
+                        borderRadius: 8,
+                        fontSize: 20,
+                        textAlign: 'center',
+                    }}
+                        onChangeText={(input) => setFirstValue(parseInt(input))}
+                    />
+                    <Text style={{ fontSize: 15, marginHorizontal: 15 }}>{''}</Text>
+                    <TextInput placeholder='' keyboardType='numeric' style={{
+                        borderColor: 'grey',
+                        borderBottomWidth: 1,
+                        backgroundColor: 'rgba(0,0,0,.1)',
+                        maxHeight: 70,
+
+                        maxWidth: 70,
+                        width: 70,
+                        borderRadius: 8,
+                        fontSize: 20,
+                        textAlign: 'center',
+                    }}
+                        onChangeText={(input) => setSecondValue(parseInt(input))}
+                    />
+
+                </View>
+
             </View>
-        </>
+
+            <View style={{ flex: 1, maxHeight: 200, maxWidth: 300, }}>
+
+                <Text numberOfLines={1} style={{ fontSize: 25, }}>Addition: {AddOperation({ firstNumber: firstValue, secondNumber: secondValue })}</Text>
+                <Text numberOfLines={1} style={{ fontSize: 25, }}>Subtraction: {SubtractOperation({ firstNumber: firstValue, secondNumber: secondValue })} </Text>
+                <Text numberOfLines={1} style={{ fontSize: 25, }}>Division: {DivideOperation({ firstNumber: firstValue, secondNumber: secondValue })} </Text>
+                <Text numberOfLines={1} style={{ fontSize: 25, }}>Multiplication: {MultiplyOperation({ firstNumber: firstValue, secondNumber: secondValue })} </Text>
+
+            </View>
+
+        </View>
     )
 }
 
